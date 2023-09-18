1dff611d07ca20ec740a6b06b1f3e7dc80abf79d
randeep007 <randeepkabir@gmail.com>
2023-09-18 12:02:42 +0530

    hogya bhy

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 import React, { useState } from "react";
 import axios from "axios";
-// import { css } from "@emotion/react";
 import { ClipLoader } from "react-spinners";
+
+// Flatten nested data into a flat array of key-value pairs
+const flattenData = (data) => {
+  let flatData = [];
+
+  const flatten = (obj, parentKey = "") => {
+    if (typeof obj !== "object") {
+      // Handle unexpected data here, log an error, or return an empty array.
+      return;
+    }
+
+    for (const key in obj) {
+      if (typeof obj[key] === "object") {
+        flatten(obj[key], parentKey + key + ".");
+      } else {
+        flatData.push({ key: parentKey + key, value: obj[key] });
+      }
+    }
+  };
+
+  flatten(data); // Call the flatten function to populate flatData
+  return flatData; // Return the flattened data
+};
+
+// Render flattened data as SEO cards
+const renderFlatData = (flatData) => {
+  return flatData.map((item) => (
+    <div key={item.key} className="seo-card">
+      <h2 className="seo-card-title">{item.key}</h2>
+      <p className="seo-card-text">{item.value}</p>
+    </div>
+  ));
+};
 
 function App() {
   const [url, setUrl] = useState("");
   const [result, setResult] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
-  // const override = css`
-  //   display: block;
-  //   margin: 0 auto;
-  //   border-color: red;
-  // `;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,71 +56,38 @@ function App() {
     }
   };
 
-  const renderData = (data) => {
-    if (!data) {
-      return null;
-    }
-
-    return Object.entries(data).map(([key, value]) => (
-      <div key={key} className="seo-card-container">
-        {" "}
-        {/* <div className="seo-card" style={{width:"18rem"}}> */}
-        <div className="seo-card">
-          {" "}
-          <h2 className="text-center">{key}</h2>
-          {typeof value === "object" ? renderData(value) : <p className="text-center">{value}</p>}
-        </div>
-      </div>
-    ));
-  };
-
   return (
-    <section>
-      <div className="container-fluid">
-        <div className="form-content">
-          <div className="input-container d-flex justify-content-center ">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                className="input"
-                placeholder="Enter any desired URL"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-              />
-              <button className="btn mx-2" type="submit">
-                Check SEO
-              </button>
-            </form>
+    <section className="app">
+      <div className="container">
+        <h1 className="title">SEO Checker</h1>
+        <form className="input-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter any desired URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button className="btn" type="submit">
+            Check SEO
+          </button>
+        </form>
+
+        {isLoading ? (
+          <div className="loader">
+            <ClipLoader size={50} color={"#123abc"} loading={isLoading} />
           </div>
+        ) : null}
 
-          {isLoading ? (
-            <div className="ls">
-              <div className="loading-spinner">
-                <ClipLoader
-                  // css={override}
-                  size={50}
-                  color={"#123abc"}
-                  loading={isLoading}
-                />
-              </div>
+        {result && (
+          <div className="results">
+            <h2 className="results-title">SEO Results</h2>
+            <hr className="divider" />
+            <div className="seo-card-container">
+              {renderFlatData(flattenData(result))}
             </div>
-          ) : null}
-
-          {result && (
-            <div>
-              <h1 className="font-weight-bolder text-center mt-5">
-                <em>SEO Results</em>
-              </h1>
-              <hr />
-              <div class="container mt-5">
-                {/* <div class="row row-cols-5 row-cols-lg-5 g-2 g-lg-3"> */}
-                <div class="row ">
-                  <div className="col seo-cards">{renderData(result)}</div>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </section>
   );
